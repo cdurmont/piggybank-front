87198778fe52f91ad1968fe8e52f607f258119b2
Cédric Durmont <cedric@durmont.net>
2022-12-09 14:56:43 +0100

    Revert "version bump nginx proxy issues for backend calls"
    This reverts commit 8b06c021

--- envvars.sh
+++ envvars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 envsubst < /usr/share/nginx/html/assets/config/config.prod.json > /usr/share/nginx/html/assets/config/config.json
-envsubst "\$PIGGYBANK_BACKEND_URL" < /temp/default.conf > /etc/nginx/templates/default.conf.template
+envsubst "\$PIGGYBANK_BACKEND_URL" < /temp/default.conf > /etc/nginx/conf.d/default.conf
 exec "$@"
